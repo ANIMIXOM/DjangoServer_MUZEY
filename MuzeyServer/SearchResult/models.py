142b2efc3417f406887482c9bf469f7bcfc2856e
ANIMIXOM <maximshlyamnev@yandex.ru>
2025-02-28 12:43:32 +0300

    Подразделение предприятий/rem

--- MuzeyServer/SearchResult/models.py
+++ MuzeyServer/SearchResult/models.py
@@ -8,23 +8,28 @@ CHOICES_PREDPRIYATIYA = [
     ('Ludinovokabel', 'Людиновокабель'),
     ('Shveynaya_fabrika', 'Швейная фабрика'),
     ('OEZ', 'ОЭЗ'),
-    ("other", 'Другое'),
     ("SCHZ", 'СЧЗ'),
+    ("other", 'Другое'),
 ]
 class Person(models.Model):
     full_name = models.CharField(max_length=255, verbose_name="Полное ФИО")
     nazvanie = models.CharField(
         max_length=50,
         choices=CHOICES_PREDPRIYATIYA,
-        verbose_name="Название предприятия",
+        verbose_name="название предприятия",
         unique=True  # Добавил unique, чтобы названия не повторялись. Уберите, если нужно.
     )
     biography = models.TextField(blank=True, verbose_name="Биография/Информация") # позволяет пустые значения
-    image1 = models.ImageField(blank=True, upload_to='static/assets/images')
-    image2 = models.ImageField(blank=True, upload_to='static/assets/images')
-    image3 = models.ImageField(blank=True, upload_to='static/assets/images')
-    image4 = models.ImageField(blank=True, upload_to='static/assets/images')
-    image5 = models.ImageField(blank=True, upload_to='static/assets/images')
+    image1 = models.ImageField(blank=True, upload_to='static/assets/images', verbose_name="Изображение (главное)")
+    image2 = models.ImageField(blank=True, upload_to='static/assets/images', verbose_name="Дополнительное изображение(1)")
+    image3 = models.ImageField(blank=True, upload_to='static/assets/images', verbose_name="Дополнительное изображение(2)")
+    image4 = models.ImageField(blank=True, upload_to='static/assets/images', verbose_name="Дополнительное изображение(3)")
+    image5 = models.ImageField(blank=True, upload_to='static/assets/images', verbose_name="Дополнительное изображение(4)")
+
+
+    class Meta:
+        verbose_name = "работник предприятий"
+        verbose_name_plural = "работники предприятий"
 
 
     def __str__(self):
